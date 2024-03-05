90df9331c75c698759204ff7160cfe37c2b5c1c4
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-05 14:14:24 +0000

    feat: add data for project

---
+++ src/data/difficulties.js
@@ -0,0 +1,10 @@
+const difficulties = {
+  Baby: 4,
+  Easy: 6,
+  Medium: 8,
+  Hard: 12,
+  Insane: 16,
+  Impossible: 20
+}
+
+export default difficulties;
---
+++ src/data/pokemonNames.js
@@ -0,0 +1,24 @@
+const pokemonNames = [
+  "pikachu",
+  "charizard",
+  "bulbasaur",
+  "squirtle",
+  "eevee",
+  "jigglypuff",
+  "snorlax",
+  "gengar",
+  "mewtwo",
+  "lapras",
+  "psyduck",
+  "magikarp",
+  "vulpix",
+  "arcanine",
+  "machamp",
+  "gyarados",
+  "alakazam",
+  "scyther",
+  "electabuzz",
+  "dragonite"
+];
+
+export default pokemonNames;
